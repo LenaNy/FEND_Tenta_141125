66d550b8a6224e9afb3a7ef982de161a5e548835
HenkeHulk <hhaggbom@gmail.com>
2014-11-25 09:24:16 +0100

    Adding Routing into app.js + adding NewGenresController.js and ReadGenresController.js + adding controller link to index.html

--- TentaFEND141125/app/app.js
+++ TentaFEND141125/app/app.js
@@ -1,1 +1,37 @@
-﻿
+﻿var app = angular.module('TentaFEND', [ngRoute]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/',
+        {
+            templateUrl: "/app/partials/NewGenres.html",
+            controller: "NewGenresController"
+        })
+        .when('/Read', {
+            templateUrl: "/app/partials/ReadGenres.html",
+            controller: "ReadGenresController"
+        });
+});
+
+//var app = angular.module('MyApp', ['ngRoute']);
+
+//app.config(function ($routeProvider) {
+//    $routeProvider.when('/',
+//        {
+//            templateUrl: "/app/partials/NewAuthor.html",
+//            controller: "NewAuthorsController"
+//        })
+//    .when('/List',
+//        {
+//            templateUrl: "/app/partials/ListAuthors.html",
+//            controller: "ListAuthorsController"
+//        })
+//    .when('/Edit',
+//        {
+//            templateUrl: "/app/partials/EditAuthors.html",
+//            controller: "EditAuthorsController"
+//        });
+//});
+
+//app.controller('NewAuthorsController', NewAuthorsController)
+//    .controller('ListAuthorsController', ListAuthorsController)
+//    .controller('EditAuthorsController', EditAuthorsController);
